b61553c20b8a13788bbf78d980a71f487bb9c535
Diana Stepanova <diana@MacBook-Air-Diana.local>
2025-04-24 19:33:01 +0500

    add React Google Charts PieChart example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Chart } from 'react-google-charts';
 
 function App() {
+  const data = [
+    ['Task', 'Hours per Day'],
+    ['Work', 9],
+    ['Eat', 2],
+    ['Commute', 2],
+    ['Read', 2],
+    ['Sleep', 7],
+  ];
+
+  const options = {
+    title: 'My Daily Activities',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '80%', margin: '50px auto' }}>
+      <h2>Pie Chart Example (Google Charts)</h2>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width="100%"
+        height="400px"
+      />
     </div>
   );
 }
